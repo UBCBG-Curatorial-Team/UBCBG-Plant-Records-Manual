7fac5baec9e33e00e198ab992f0a7f1301dc8d58
UBC Botanical Garden Curatorial Team <curatorialgardenteam@gmail.com>
2026-08-07 16:10:28 -0700

    Finalized window run files

--- run/windows_run.bat
+++ run/windows_run.bat
@@ -1,7 +1,4 @@
 @echo off
 cd [path to UBCBG-Plant-Records-Manual directory]
-echo initializing server in other window
 start .\run\extra_bats\extra.bat
-echo please wait while initialization occurs
-timeout 20
 start .\run\extra_bats\extra2.bat
